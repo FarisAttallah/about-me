98b4f64f151f05ef65f347fc28d3ce7bb6ea094c
FarisAttallah <126489501+FarisAttallah@users.noreply.github.com>
2024-12-17 17:32:21 -0500

    fixed api links

--- src/pages/projects/components/project.jsx
+++ src/pages/projects/components/project.jsx
@@ -8,7 +8,7 @@ const Project = ({ projectId }) => {
   useEffect(() => {
     const fetchData = async () => {
       console.log(process.env.NEXT_PUBLIC_API_BASE_URL)
-      const apiUrl = process.env.NEXT_PUBLIC_API_BASE_URL + '/projects/';
+      const apiUrl = process.env.NEXT_PUBLIC_API_BASE_URL + 'api/projects/';
 
       const res = await fetch(apiUrl); // Replace with your API route
       const projectData = await res.json();
